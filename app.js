70c7e0b207d7fca47ed3846c2d708503fc9531cc
Clay Diffrient <cdiffrient@instructure.com>
2015-02-15 09:37:13 -0700

    Fixes for CORS issues

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'jade');
 // Enable CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token");
   next();
 });
 
--- utils/requireAuthentication.js
+++ utils/requireAuthentication.js
@@ -4,9 +4,10 @@
  * It's also not very scalable, but works for now.
  */
 var requireAuthentication = function (req, res, next) {
-  var token = req.query.token || req.headers['X-Access-Token'];
+  var token = req.query.token || req.headers['x-access-token'];
 
-  if (token === process.env.ACCESS_TOKEN) {
+  if ((token === process.env.ACCESS_TOKEN) ||
+     (req.method === 'OPTIONS')) {
     return next();
   }
   res.status(401).json({
