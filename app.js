e02ef17ace226fbecd39e73e8e9d8388904816ee
Clay Diffrient <cdiffrient@instructure.com>
2015-02-18 18:43:49 -0700

    Fixes a slight problem with CORS

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('view engine', 'jade');
 // Enable CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Access-Token");
   next();
 });
